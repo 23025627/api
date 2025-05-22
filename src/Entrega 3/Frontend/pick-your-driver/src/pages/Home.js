8876f38f661d227d83855ffe66d799ed62dd6086
Leonardo de Souza Mouzinho <leonardosouzamouzinho4554@gmail.com>
2025-05-22 03:31:36 -0300

    site online e api conectada

--- src/Entrega 3/Frontend/pick-your-driver/src/pages/Home.js
+++ src/Entrega 3/Frontend/pick-your-driver/src/pages/Home.js
@@ -48,7 +48,7 @@ function Home() {
       // Fazendo a requisição para a API Flask
       setLoading(true); 
       try {
-        const response = await fetch('http://localhost:5000/api/prever', {
+        const response = await fetch('https://api-1-t882.onrender.com/api/prever', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
